7d0b6534e1edd5068eb5869a0275a53648fcc771
AdityaP183 <adityaprasad1828@gmail.com>
2025-06-20 03:20:14 +0530

    16.1: bug fixes

--- src/app/dashboard/[userId]/page.tsx
+++ src/app/dashboard/[userId]/page.tsx
@@ -1,16 +1,26 @@
 import { ChartAreaInteractive } from "@/features/dashboard/components/chart-area-interactive";
 import DataTable from "@/features/dashboard/components/data-table";
 import { SectionCards } from "@/features/dashboard/components/section-cards";
+import { currentUser } from "@clerk/nextjs/server";
+import { fetchQuery } from "convex/nextjs";
+import { api } from "../../../../convex/_generated/api";
 
 export default async function Dashboard() {
+	const user = await currentUser();
+	const dbUser = await fetchQuery(api.users.getUser, {
+		userId: user?.id || "",
+	});
+
 	return (
 		<div className="flex flex-1 flex-col">
 			<div className="@container/main flex flex-1 flex-col gap-2">
 				<div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
 					<SectionCards />
-					<div className="px-4 lg:px-6">
-						<ChartAreaInteractive />
-					</div>
+					{dbUser?.role === "admin" && (
+						<div className="px-4 lg:px-6">
+							<ChartAreaInteractive />
+						</div>
+					)}
 					<DataTable />
 				</div>
 			</div>
